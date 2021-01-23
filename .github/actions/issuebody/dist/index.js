7238345ebdee1e61dd4c3e629212b71f58c80ed5
Milan Otáhal <su@milous.cz>
2021-01-23 01:59:09 +0100

    fix

--- .github/actions/issuebody/dist/index.js
+++ .github/actions/issuebody/dist/index.js
@@ -26,7 +26,7 @@ try {
 	// const issueBody = 'octocat zaslaný přes ghactions ' + (new Date()).toTimeString();
 	console.log(`ssueNumber ${repo}!`);
 	console.log(event);
-	console.log(event.number);
+	console.log(JSON.parse(event).number);
 
 	octokit
 		.request(`GET /repos/milous/actions-pr-issue-comment/issues/${issueNumber}`)
