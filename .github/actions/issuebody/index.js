e1dfd9226f06c5dad3687ac0c2887e1e07fd4e14
Milan Otáhal <su@milous.cz>
2021-01-25 13:01:13 +0100

    fixup

--- .github/actions/issuebody/index.js
+++ .github/actions/issuebody/index.js
@@ -7,42 +7,25 @@ try {
 	// npm i -g @zeit/ncc
 	const token = core.getInput('token');
 	const event = JSON.parse(core.getInput('event'));
-	const issueNumber = core.getInput('issue-number');
 	const pattern = core.getInput('issue-regexp');
+
 	const prNumber = event.number;
 	const prBody = event.pull_request.body;
 	const repo = event.repository.full_name;
 
-	console.log(prBody);
-	// console.log(regexp);
-
 	const matches = prBody.match(new RegExp(pattern, "g"));
 
+	core.setOutput("pr-number", prNumber);
+	core.setOutput("pr-body", prBody);
+
 	if (matches === null) {
-		console.log('Nepodařilo se načíst číslo issue z PR message');
+		core.setOutput("issue-number", '');
 
 		return;
 	}
 
 	const issueNumberDetected = matches[0].replace(/\D/g, "");
-
-	console.log('issue number z pr body ' + issueNumberDetected);
-
-
-
-	// // var re = new RegExp("Issue: #([0-9]+)", "g");
-	// myString.match(re)[0].replace(/\D/g, "");
-	//
-	// if ()
-	//
-	// 	var myString = "something Issue: # 1234 format_abc";
-	// var re = new RegExp("Issue: #([0-9]+)", "g");
-	// myString.match(re);
-	// 	[0].replace(/\D/g, "");
-
-	// console.log(prBody.match(re));
-	// const issueBodyPrefix = core.getInput('issue-body-prefix');
-	// const issueBodySuffix = core.getInput('issue-body-suffix');
+	core.setOutput("issue-number", issueNumberDetected);
 
 	const octokit = new github.getOctokit(token);
 	octokit
@@ -55,17 +38,6 @@ try {
 			})
 		})
 	;
-
-
-	console.log(`IssueNumber ${issueNumber}!`);
-	console.log(`PrNumber ${prNumber}!`);
-	const time = (new Date()).toTimeString();
-	//
-	core.setOutput("time", time);
-	// // Get the JSON webhook payload for the event that triggered the workflow
-	// const payload = JSON.stringify(github.context.payload, undefined, 2)
-	// console.log(`The event payload: ${payload}`);
-
 } catch (error) {
 	core.setFailed(error.message);
 }
